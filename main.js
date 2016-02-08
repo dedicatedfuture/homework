7d16c39988791d5bd402400c61756ac311f3a147
Bill Zerega <zerega85@gmail.com>
2016-02-08 16:24:23 -0500

    homework mostly done Feb 8

---
+++ main.js
@@ -0,0 +1,51 @@
+var a = "bill"; //declaring a variable, string
+
+1 // number
+"bill" //string
+true //boolean
+
+
+var array = [1, 2, 3, 4] //declaring an array
+
+function add(x, y){
+  console.log(x + y);
+}
+function subtract(x, y){
+  console.log(x - y);
+}
+function multiply(x, y){
+  alert(x * y)
+}
+
+function check(x){
+  if(x < 10){
+    alert(x + " is less than 10!");
+  } else {
+    console.log(x + " is greater than 10, or 10 ");
+  }
+}
+
+function kissass(){
+  console.log("New York Code and Design Academy rules!");
+}
+
+function whatname(x){
+  console.log("You have entered name:" + x);
+}
+
+var poolgames = [["9-ball", "8-ball"], ["straight pool", "cut-throat"]]
+console.log(poolgames[1][0]);
+
+
+function checkCities(){
+  var paCities = ["Warren", "Washington", "Wilkes-Barre", "Williamsport", "York"];
+  var shortCities = [];
+  for (var i = 0; i <= paCities.length; i++){
+    var city = paCities[i];
+    if (city.length < 8) {
+      shortCities.push(paCities[i]);
+    }
+  }
+  console.log(shortCities);
+}
+
