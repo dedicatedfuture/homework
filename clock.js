fb238909005bee2775cbf99fc3ecf02525bc5d39
Bill Zerega <zerega85@gmail.com>
2016-02-11 14:07:43 -0500

    updating clock, not done formatting

---
+++ clock.js
@@ -0,0 +1,87 @@
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  var grab = document.getElementById("clock").innerHTML = h + ":" + m + ":" + s;
+  var t = setTimeout(startTime, 500);
+}
+function checkTime(i){
+  if(i<10) {
+    i = "0" + i;
+  };
+  return i;
+}
+
+function checkMinute(){
+
+}
+
+// 1. change the color to black if it is an odd minute another color if it is //
+//even minute
+//2. Change the background to a random color every 5 minutes
+//3.Change the text color to a different color of the clock every hour selected
+//from an array of colors
+
+$(document).ready(function(){
+  var currentHour = 0;
+  var counter = 0;
+  var cssColor = ['red', 'blue', 'yellow', 'green', 'orange', 'pink', 'purple', 'lightblue', 'lightgreen', 'brown', 'gray']
+  var randomNum2 = Math.floor(Math.random()* (10 - 0)) + 0;
+
+
+
+  function displayTime(){
+     var date = new Date;
+     var hours = date.getHours();
+     var minutes = date.getMinutes();
+     var seconds = date.getSeconds();
+     var milliseconds = date.getMilliseconds();
+
+     $('#hours').text(hours);
+     $("#minutes").text(formatMinutesAndSeconds(minutes));
+     $("#seconds").text(formatMinutesAndSeconds(seconds));
+     $("#milliseconds").text(milliseconds);
+
+     ifMinOdd(minutes);
+     setInterval(ifFiveMin(minutes), 300000);
+     ifHour(hours);
+  }
+
+  function formatMinutesAndSeconds(value) {
+     if (value < 10){
+        return '0' + value;
+     } else {
+        return value;
+     }
+  }
+
+  function ifMinOdd(min){
+    if (min % 2 === 0){
+      $('#minutes').css('color', 'red');
+    }else {
+      $('#minutes').css('color', 'black');
+    }
+  }
+
+  function ifFiveMin(min){
+     $('#timeContainer').css('background-color', cssColor[randomNum2]);
+  }
+
+  function ifHour(newHour){
+    var randomNum = Math.floor(Math.random()* (10 - 0)) + 0;
+    if(newHour !== currentHour){
+      $('#hours').css('background-color', cssColor[randomNum]);
+      currentHour = newHour;
+    }
+  }
+
+
+
+  window.setInterval(displayTime, 1);
+
+
+
+})
